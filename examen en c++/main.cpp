68d624104b635dc767d63a46ed14b89054cafa20
csantillgar <csantill@myuax.com>
2023-12-14 12:57:01 +0100

    examen en c++

--- examen en c++/main.cpp
+++ examen en c++/main.cpp
@@ -2,8 +2,15 @@
 #include "Asistencia.h"
 
 
-int main()
-{
+int main(){
+    Estudiante estudiante;
+    estudiante.nombre = "Cintia Santillan";
+    estudiante.edad = 18;
+    estudiante.nota = 5;
+    estudiante.agregarAsignatura("Programacion");
+    estudiante.agregarAsignatura("Estructura de datos");
+    estudiante.mostrarEstudiante();
+
 
     return 0;
 }
